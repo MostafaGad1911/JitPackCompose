2bbafba0645669ed18c8356cb6ad490192d7167e
E <mostafa.gad@appssquare.com>
2023-01-14 00:05:13 +0200

    reset master .

--- app/src/main/java/mostafagad/projects/JitPackCompose/MainActivity.kt
+++ app/src/main/java/mostafagad/projects/JitPackCompose/MainActivity.kt
@@ -1,167 +1,40 @@
 package mostafagad.projects.JitPackCompose
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import mostafagad.projects.JitPackCompose.ui.theme.ComposeFirstTheme
-import mostafagad.projects.compose.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                DiceRollerApp()
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DiceRollerApp() {
-    Calculator(
-    )
+fun DefaultPreview() {
+    ComposeFirstTheme {
+        Greeting("Android")
+    }
 }
-
-@Composable
-fun Calculator() {
-    var firstInput by rememberSaveable  {
-        mutableStateOf(
-            ""
-        )
-    }
-
-    var secondInput by rememberSaveable  {
-        mutableStateOf(
-            ""
-        )
-    }
-
-    var sum = 0f
-    if (firstInput.isNotEmpty() && secondInput.isNotEmpty())
-        sum = secondInput.toFloatOrNull()?.let { firstInput.toFloatOrNull()?.plus(it)}!!
-
-    var sub = 0f
-    if (firstInput.isNotEmpty() && secondInput.isNotEmpty())
-        sub = secondInput.toFloatOrNull()?.let { firstInput.toFloatOrNull()?.minus(it)}!!
-
-    var mul = 0f
-    if (firstInput.isNotEmpty() && secondInput.isNotEmpty())
-        mul = firstInput.toFloatOrNull()?.times(secondInput.toFloatOrNull()!!)!!
-
-    var div = 0f
-    if (firstInput.isNotEmpty() && secondInput.isNotEmpty())
-        div = secondInput.toFloatOrNull()?.let { firstInput.toFloatOrNull()?.div(it) }!!
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .wrapContentSize(Alignment.Center)
-            .padding(15.dp)
-    ) {
-        val context = LocalContext.current
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = firstInput,
-            onValueChange = { input ->
-                firstInput = input
-            },
-            label = {
-                Text(stringResource(id = R.string.first_num))
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = secondInput,
-            onValueChange = { input ->
-                secondInput = input
-            },
-            label = {
-                Text(stringResource(id = R.string.second_num))
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(modifier = Modifier.fillMaxWidth(), onClick = { sum.toString().mToast(context) }) {
-            Text(text = stringResource(id = R.string.sum))
-        }
-
-        Spacer(modifier = Modifier.height(5.dp))
-
-        Button(modifier = Modifier.fillMaxWidth(), onClick = { sub.toString().mToast(context) }) {
-            Text(text = stringResource(id = R.string.sub))
-        }
-
-        Spacer(modifier = Modifier.height(5.dp))
-
-        Button(modifier = Modifier.fillMaxWidth(), onClick = { mul.toString().mToast(context) }) {
-            Text(text = stringResource(id = R.string.mul))
-        }
-
-        Spacer(modifier = Modifier.height(5.dp))
-
-        Button(modifier = Modifier.fillMaxWidth(), onClick = { div.toString().mToast(context) }) {
-            Text(text = stringResource(id = R.string.div))
-        }
-    }
-
-    @Composable
-    fun RollImg(modifier: Modifier = Modifier) {
-        var result by rememberSaveable  {
-            mutableStateOf(1)
-        }
-        val img = when (result) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        Column(
-            modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(painter = painterResource(img), contentDescription = "1")
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { result = (1..6).random() }) {
-                Text(text = stringResource(id = R.string.roll))
-            }
-
-        }
-    }
-
-
-}
-
-
-fun String.mToast(context: Context) {
-    Toast.makeText(context, this, Toast.LENGTH_LONG).show()
-}
-
